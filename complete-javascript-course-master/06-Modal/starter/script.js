04b865e135ddcccee94b891bfc27fb2621b70fb7
Emmanuelsam124 <58230732+Emmanuelsam124@users.noreply.github.com>
2025-07-24 02:42:08 +0100

    Update script.js

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,11 +1,18 @@
 'use strict';
+
+// Select all elenents I need for this project
 const modalEl = document.querySelector('.modal');
 const overlayEl = document.querySelector('.overlay');
 const closeModalEl = document.querySelector('.close-modal');
 const modalButtons = document.querySelectorAll('.show-modal');
 
+// Loop through all of the modal buttons
 for (let button = 0; button < modalButtons.length; button++) {
   modalButtons[button].addEventListener('click', function () {
+    //EventLisenefor each button
+    // Show the modal and overlay when a button is clicked
+    // Remove the hidden class from the modal and overlay
+    // This will make them visible
     document.querySelector('.modal').classList.remove('hidden');
     document.querySelector('.overlay').classList.remove('hidden');
   });
@@ -20,7 +27,6 @@ closeModalEl.addEventListener('click', closeModal);
 overlayEl.addEventListener('click', closeModal);
 
 const escKeyToCloseOverlay = function (event) {
-  debugger;
   if (event.key === 'Escape') {
     closeModal();
   }

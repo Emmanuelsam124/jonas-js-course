c1baf70b338e8a300051ce57ca31a3d2d1f6440f
Emmanuelsam124 <58230732+Emmanuelsam124@users.noreply.github.com>
2025-07-24 01:52:42 +0100

    Built the modal project on my own before watchign the video

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+const modalButtons = document.querySelectorAll('.show-modal');
+for (let button = 0; button < modalButtons.length; button++) {
+  modalButtons[button].addEventListener('click', function () {
+    document.querySelector('.modal').classList.remove('hidden');
+    document.querySelector('.overlay').classList.remove('hidden');
+  });
+}
+
+const closeModal = function () {
+  document.querySelector('.modal').classList.add('hidden');
+  document.querySelector('.overlay').classList.add('hidden');
+};
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+document.querySelector('.overlay').addEventListener('click', closeModal);
+
+const escKeyToCloseOverlay = function (event) {
+  debugger;
+  if (event.key === 'Escape') {
+    document.querySelector('.modal').classList.add('hidden');
+    document.querySelector('.overlay').classList.add('hidden');
+  }
+};
+
+document.addEventListener('keydown', escKeyToCloseOverlay);

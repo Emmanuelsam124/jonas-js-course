82d478ec7eb9da58411620026145d79fb4237c57
Emmanuelsam124 <58230732+Emmanuelsam124@users.noreply.github.com>
2025-07-28 23:36:59 +0100

    Dice roll functionality
    Added dice roll functionality and player switching

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -3,8 +3,42 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+  // diceEl.textContent = dice;
+  // 3. Check for rolled 1: if true, switch to next playerCan you generate videos
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // document.getElementById(`current--${activePlayer}`).textContent =
+    //   currentScore;
+  }
+});
